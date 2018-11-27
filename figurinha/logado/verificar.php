30c76f3534f6a8ae2c0d23849a8c2a294322baa4
LehNeves <lneves997@gmail.com>
2018-11-27 00:44:40 -0200

    Arquivo de verificação de login

---
+++ figurinha/logado/verificar.php
@@ -0,0 +1,7 @@
+<?php
+    session_start();
+    if($_SESSION['nome'] == ''){
+        echo '<script>alert("Faça o login novamente!");window.location.href="../index.html";</script>';
+    }
+    $nomeUser = $_SESSION['nome'];
+?>

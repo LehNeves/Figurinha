f44ade3fe4a4d9f4e7b7faf7ff2cb59b42f0228f
LehNeves <lneves997@gmail.com>
2018-11-28 15:20:48 -0200

    Cirada a função gerarUltimoAcesso

--- figurinha/logado/index.php
+++ figurinha/logado/index.php
@@ -1,9 +1,10 @@
 <?php
     include 'verificar.php';
-    include '../bd/conexao.php';
     echo "<script>alert('Bem vindo $nomeUser');</script>";
 
-    $update = "UPDATE `usuarios` SET `ultimoAcesso` = '2018/11/28 15:08:42' WHERE `nome` = 'admin'";
-    print_r($update);   
+    if(!gerarUltimoAcesso($dateTime, $conexao)){
+        echo "<script>alert('Erro ao passar Ultimo Acesso');</script>";
+    }
+    
 
 ?>
--- figurinha/logado/verificar.php
+++ figurinha/logado/verificar.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    include '../bd/conexao.php';
     if($_SESSION['nome'] == ''){
         echo '<script>alert("Faça o login novamente!");window.location.href="../index.html";</script>';
     }
@@ -7,5 +8,14 @@
 
     date_default_timezone_set('America/Sao_Paulo');
     $dateTime = date('Y/m/d H:i:s', time());
+
+    function gerarUltimoAcesso($dateTime, $conexao){
+        $update = "UPDATE `usuarios` SET `ultimoAcesso` = '$dateTime' WHERE `nome` = 'admin'"; 
+
+        $resultado =  mysqli_query($conexao, $update);
+    
+        return $resultado;
+    }
+
 
 ?>

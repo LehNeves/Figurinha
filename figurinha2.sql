8e95750291eda01fabc1c63ff3541ebf05dcf138
LehNeves <lneves997@gmail.com>
2018-12-06 16:16:05 -0200

    !

--- figurinha2.sql
+++ figurinha2.sql
@@ -1,42 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 01-Dez-2018 às 18:57
--- Versão do servidor: 10.1.36-MariaDB
--- versão do PHP: 7.2.11
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `figurinha`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `figurinhas`
---
 
 CREATE TABLE `figurinhas` (
   `id` int(11) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `caminho` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `figurinhas`
---
 
 INSERT INTO `figurinhas` (`id`, `nome`, `caminho`) VALUES
 (1, 'Squirtle', 'squirtle.png'),
@@ -69,11 +40,11 @@ INSERT INTO `figurinhas` (`id`, `nome`, `caminho`) VALUES
 (28, 'Umbreon', 'umbreon.png'),
 (29, 'Wooper', 'wooper.png');
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
+CREATE TABLE `troca` (
+  `id` int(11) NOT NULL,
+  `figurinhas_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
@@ -85,65 +56,68 @@ CREATE TABLE `usuarios` (
   `ultimaFigurinha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Extraindo dados da tabela `usuarios`
---
-
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `login`, `senha`, `ultimoAcesso`, `ultimaFigurinha`) VALUES
-(1, 'admin', 'admin', 'admin', 'admin', '2018-12-01 15:56:23', '2018-12-01'),
-(2, 'Leandro', 'leandro@email.com', 'leandro', 'leandro', '2018-12-01 15:45:26', '2018-12-01');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuariosfigurinhas`
---
+(1, 'admin', 'admin', 'admin', 'admin', '2018-12-06 16:09:55', '2018-12-06'),
+(2, 'Leandro', 'leandro@email.com', 'leandro', 'leandro', '2018-12-05 12:49:05', '2018-12-05'),
+(3, 'melk', 'melk', 'melk', 'melk', '2018-12-05 12:48:10', '2018-12-05'),
+(4, 'vagner', 'vagner', 'vagner', 'vagner', '2018-12-05 13:15:28', '2018-12-05'),
+(5, 'Ze Da Favela', 'zedafavela@zefavela.com', 'ZeFaveludo', '123Favela', '2018-12-01 17:01:49', '2018-12-01'),
+(6, 'Aline', 'reis.araujo.a@hotmail.com', 'Aline', '123456', '2018-12-05 14:07:28', '2018-12-05'),
+(7, 'a', 'a', 'a', 'a', '2018-12-06 16:10:09', '2018-12-05');
 
 CREATE TABLE `usuariosfigurinhas` (
   `id` int(11) NOT NULL,
   `usuarios_id` int(11) NOT NULL,
-  `figurinha_id` int(11) NOT NULL
+  `figurinha_id` int(11) NOT NULL,
+  `disponivel` tinyint(2) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
+INSERT INTO `usuariosfigurinhas` (`id`, `usuarios_id`, `figurinha_id`, `disponivel`) VALUES
+(1, 18, 7, 1),
+(2, 18, 20, 1),
+(3, 18, 14, 1),
+(4, 18, 13, 1),
+(5, 18, 7, 1),
+(6, 7, 23, 1),
+(7, 1, 16, 1),
+(8, 7, 14, 0),
+(9, 7, 12, 0),
+(10, 7, 10, 1),
+(11, 1, 19, 0),
+(12, 1, 14, 0),
+(13, 1, 20, 0),
+(14, 7, 22, 1),
+(15, 1, 5, 0),
+(16, 1, 5, 0),
+(17, 1, 10, 0),
+(18, 1, 13, 0),
+(19, 1, 4, 1),
+(20, 1, 10, 0);
 
---
--- Indexes for table `figurinhas`
---
 ALTER TABLE `figurinhas`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `usuarios`
---
+ALTER TABLE `troca`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idTrocaUsurarios` (`usuario_id`),
+  ADD KEY `idTrocaFigurinhas` (`figurinhas_id`);
+
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `usuariosfigurinhas`
---
 ALTER TABLE `usuariosfigurinhas`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE `troca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `usuarios`
---
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- AUTO_INCREMENT for table `usuariosfigurinhas`
---
 ALTER TABLE `usuariosfigurinhas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+ALTER TABLE `troca`
+  ADD CONSTRAINT `idTrocaFigurinhas` FOREIGN KEY (`figurinhas_id`) REFERENCES `figurinhas` (`id`),
+  ADD CONSTRAINT `idTrocaUsurarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
